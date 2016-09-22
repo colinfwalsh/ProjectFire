4b44d3b85570a21ccc4bc51d5f905f0cb8197879
colinfwalsh <colinfwalsh@gmail.com>
2016-09-22 17:37:14 -0400

    Started basic layout for the front-end

---
+++ projectFire/projectFire/IntroViewController.swift
@@ -0,0 +1,14 @@
+//
+//  IntroViewController.swift
+//  projectFire
+//
+//  Created by Colin Walsh on 9/22/16.
+//  Copyright © 2016 Colin Walsh. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class IntroViewController: UIViewController {
+
+}
